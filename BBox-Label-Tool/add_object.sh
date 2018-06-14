6fc66a1a0b56d70d27a192cd3d1b826d95cf0d73
Lucas Maurice <lucas.maurice@outlook.com>
2018-06-14 00:28:44 -0400

    Add creation of class folders

--- BBox-Label-Tool/add_object.sh
+++ BBox-Label-Tool/add_object.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 echo "This tool is for prepare a new class for labeling."
 echo "You will have to enter the class id and name, and the video to convert in images."
-read -p "Enter the class ID: " -r
-class_id=$REPLY
-read -p "Enter the class label: " -r
-class_label=$REPLY
-echo "The class $class_label got the id $class_id"
+echo "Enter the class ID (integer):"
+
+# Get class id, check value.
+ID=""
+while [[ ! $ID =~ ^[0-9]+$ ]]; do
+    read ID
+    if [[ ! $ID =~ ^[0-9]+$ ]]; then
+        echo "This is not an Id. Type an integer!"
+    fi
+done
+printf -v CLASS_ID "%03d" $ID
+
+echo "Enter the class label: "
+read CLASS_LABEL
+echo "The class \"$CLASS_LABEL\" will be created with the id $CLASS_ID"
+
+# Create related folders
+mkdir -p ./Images/$CLASS_ID
+mkdir -p ./Examples/$CLASS_ID
+mkdir -p ./Labels/$CLASS_ID
 
 # if [[ $REPLY =~ 'yes' ]]
 # then
